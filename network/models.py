1700c076e45e1693c138b3e9eed1d2b3fac79f79
judithMarg <judithmargaritapacollalli@gmail.com>
2024-07-23 02:24:25 -0400

    add some methods for User

--- network/models.py
+++ network/models.py
@@ -10,15 +10,27 @@ class User(AbstractUser):
     def __str__(self):
         return f'{self.id}: {self.username} tiene {self.n_followers} sigue {self.n_follows}'
 
+    def increment_follow(self):
+        self.n_follows += 1
+        self.save()
+
+    def decrement_follow(self):
+        self.n_follows -= 1
+        self.save()
+        
 class Post(models.Model):
     content = models.CharField(max_length=128)
-    date = models.DateField(null=True)
-    time = models.TimeField(null=True)
+    date = models.DateField(null=True, auto_now=True)
+    time = models.TimeField(null=True, auto_now=True)
     username_post = models.ForeignKey(User, on_delete=models.CASCADE, related_name="poster")
     n_likes = models.IntegerField(default=0)
 
     def __str__(self):
         return  f'{self.id}: {self.content} by {self.username_post}'
+    
+    def edit(self, new_content):
+        self.content = new_content
+        self.save()
     
 class Follow(models.Model):
     follower_id = models.ForeignKey(User, on_delete=models.CASCADE, related_name="follower_id")
